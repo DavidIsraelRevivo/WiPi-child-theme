be4deca4ebb7b69fe3a5367f6fde1209b8ecaaaa
DavidIsraelRevivo <davidrevivotools@gmail.com>
2016-06-26 20:34:53 +0000

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+// Enqueue css files from parent theme
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+function my_theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+
+}
+
+?>
